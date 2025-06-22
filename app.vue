c15fa8072ce578d37d9aa0b3fb67ebf4871fab59
ninja <ninja@ninja.com>
2025-06-22 14:07:28 +0200

    og tags

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+useSeoMeta({
+  title: 'Hyperio',
+  ogTitle: 'Hyperio',
+  description: 'Alien shapes.',
+  ogDescription: 'Alien shapes.',
+  ogImage: 'https://hyperio.fr/home/home2-high.jpg',
+  twitterCard: 'summary_large_image'
+})
+</script>
+
 <template>
   <UApp>
     <NuxtLayout>
